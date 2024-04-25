54fa8e9813a7c86feea9dd398d26bb7c742bcc24
MinHeinKoKo <kokominhein96@gmail.com>
2024-04-25 20:25:19 +0630

    making notes policy and edit

--- resources/views/livewire/notes/show-notes.blade.php
+++ resources/views/livewire/notes/show-notes.blade.php
@@ -1,8 +1,17 @@
 <?php
 
 use Livewire\Volt\Component;
+use App\Models\Note;
 
 new class extends Component {
+
+    public function delete(string $noteId)
+    {
+        $note = Note::where('id', $noteId)->first();
+        $this->authorize('delete', $note);
+        $note->delete();
+    }
+
     public function with()
     {
         return [
@@ -23,13 +32,16 @@ new class extends Component {
         @else
             <x-button primary icon-right="plus" class="my-6" href="{{ route('notes.create') }}" wire:navigate>Create
                 Note</x-button>
-            <div class="grid grid-cols-2 gap-4">
+            <div class="grid grid-cols-3 gap-4">
                 @foreach ($notes as $note)
                     <x-card wire:key='{{ $note->id }}'>
                         <div class="flex justify-between">
-                            <a href="#" class="text-xl font-bold hover:underline hover:text-blue-500">
-                                {{ $note->title }}
-                            </a>
+                            <div>
+                                <a href="{{ route('notes.edit', $note) }}" wire:navigate class="text-xl font-bold hover:underline hover:text-blue-500">
+                                    {{ $note->title }}
+                                </a>
+                                <p class="text-xs mt-2">{{ Str::limit($note->body, 30) }}</p>
+                            </div>
                             <div class="text-xs text-gray-400">
                                 {{ \Carbon\Carbon::parse($note->send_date)->format('d/M/Y') }}
                             </div>
@@ -38,7 +50,7 @@ new class extends Component {
                             <p class="text-xs">Recipient : <span class="font-semibold">{{ $note->recipient }}</span></p>
                             <div>
                                 <x-button.circle icon="eye"></x-button.circle>
-                                <x-button.circle icon="trash"></x-button.circle>
+                                <x-button.circle icon="trash" wire:click="delete('{{ $note->id }}')"></x-button.circle>
                             </div>
                         </div>
                     </x-card>
